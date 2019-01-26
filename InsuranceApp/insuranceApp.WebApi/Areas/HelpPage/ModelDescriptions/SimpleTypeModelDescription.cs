aa7323ae958be9619364c9af3d1771330e27a3ab
Gustavo <gustavo.vargas@realself.com>
2019-01-25 22:38:17 -0600

    adding basic build blocks for the architecture

---
+++ InsuranceApp/insuranceApp.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace insuranceApp.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ InsuranceApp/insuranceApp.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="insuranceApp.WebApi.WebApiApplication" Language="C#" %>
